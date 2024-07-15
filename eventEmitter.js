9915100f39a55e2dc9ff9fa95cbee93bc1595058
Caleb Potts <Caleb.Potts@sanlam.co.za>
2024-07-15 11:55:59 +0200

    eventEmitter

---
+++ eventEmitter.js
@@ -0,0 +1,31 @@
+linkForEvents = "https://nodejs.org/docs/v20.15.1/api/events.html#emitterremovealllistenerseventname"
+
+// const { EventEmitter } = require("stream");
+
+// const emitter = new EventEmitter
+// emitter.on("foo", () => {
+//     console.log("Event foo triggered");
+// })
+// emitter.emit("foo");
+
+// const { EventEmitter } = require("stream");
+// const EVENT_NAME = "foo"
+// const emitter = new EventEmitter
+// emitter.on(EVENT_NAME, () => {
+//     console.log("Event foo triggered");
+// })
+// emitter.emit(EVENT_NAME);
+
+const {EventEmitter} = require('node:events');
+ 
+const emitter = new EventEmitter();
+ 
+const EVENT_NAME = 'join';
+ 
+// Listen to a event
+emitter.on(EVENT_NAME, () => {
+    console.log('User have joined a conversation');
+})
+  
+// Trigger a event
+emitter.emit(EVENT_NAME);
